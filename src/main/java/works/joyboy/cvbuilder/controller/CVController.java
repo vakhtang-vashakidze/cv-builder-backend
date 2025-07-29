83ab3f427f86f1b6da257267289259f145f65ba8
vakhtang-vashakidze <vvash2017@gmail.com>
2025-07-29 14:26:11 +0400

    added base support for cors

--- src/main/java/works/joyboy/cvbuilder/controller/CVController.java
+++ src/main/java/works/joyboy/cvbuilder/controller/CVController.java
@@ -6,16 +6,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import works.joyboy.cvbuilder.model.CVRequest;
 import works.joyboy.cvbuilder.service.DocumentGenerator;
 
 @Validated
 @RestController
 @RequestMapping("/cv")
+@CrossOrigin(value = "*")
 public class CVController {
 
     private final DocumentGenerator pdfGenerator;
